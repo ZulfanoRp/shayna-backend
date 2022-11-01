656469af90c6b602810f35d86efa17ef34497c76
Zulfano <zazubaza@gmail.com>
2022-11-01 21:21:14 +0700

    config auth admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [DashboardController::class, 'index']);
+Auth::routes(['register' => false]);
+
