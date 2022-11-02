7c91b9587273711844e4de67e3c05e95d8aa495d
Zulfano <zazubaza@gmail.com>
2022-11-02 16:16:19 +0700

    make CRUD for product data

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,8 @@ use App\Http\Controllers\DashboardController;
 //     return view('welcome');
 // });
 
-Route::get('/', [DashboardController::class, 'index']);
+Route::get('/', [DashboardController::class, 'index']) -> name('dashboard');
+
 Auth::routes(['register' => false]);
 
+Route::resource('products', ProductController::class);
